eb6830c653f4a3fb0bc9cff87d9c156a4d8d07ef
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-14 15:55:28 +0500

    changed

--- main.js
+++ main.js
@@ -3,33 +3,32 @@ const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result=document.getElementById("result");
 const sound = document.getElementById('sound');
 const btn = document.getElementById("search-btn");
+const searchedText=document.getElementById('searchedText');
 btn.addEventListener('click',()=>{
     let inpWord=document.getElementById("inp-word").value;
     fetch(`${url}${inpWord}`)
-        .then((response)=>response.json())
-        .then((data)=>{
-            // console.log(data);
-            result.innerHTML=`
-            <div class="word">
-                <h3>
-                    ${inpWord}
-                </h3>
-                <button onclick="playSound()" >
-                    <i class="fa-solid fa-volume-high"></i>
-                </button>
-            </div>
+    .then((response)=>response.json())
+    .then((data)=>{
+            searchedText.innerHTML=inpWord;
+            data[0].meanings.map(item=>{
+                console.log(item);
+                result.innerHTML=`
+           
             <div class="details">
-                <p>${data[0].meanings[0].partOfSpeech}</p>
+                <p>${item.partOfSpeech}</p>
                 <p>/${data[0].phonetic}/</p>
             </div>
             <p class="word-meaning">
-                ${data[0].meanings[0].definitions[0].definition}
+                ${item.definitions[0].definition}
             </p>
             <p class="word-example">
-                ${data[0].meanings[0].definitions[0].example || ""}
-            </p>`;
+                ${item.definitions[0].example || ""}
+            </p>
+            `;
+            document.getElementById('voicebtn').style.display='block';
+        })
             sound.setAttribute("src",`${data[0].phonetics[0].audio}`);
-            // console.log(sound);
+            console.log(sound);
         })
         .catch(()=>{
             result.innerHTML=`<h4 class='error'>Couldn't Find The Word</h4>`;
